4a5b067870a9c164d488000be04932cb4b57a8d3
wangkun23 <wang.kun@jinyinwu.com>
2017-09-22 10:57:04 +0800

    show all my marrage

--- wallbase-web/src/main/java/tv/wallbase/controller/HomeController.java
+++ wallbase-web/src/main/java/tv/wallbase/controller/HomeController.java
@@ -35,7 +35,7 @@ public class HomeController {
      */
     @RequestMapping(method = RequestMethod.GET)
     public String displayIndex(Model model) {
-        Pageable pageable = new Pageable(1, 50);
+        Pageable pageable = new Pageable(1, 100);
         Page<Wallpaper> page = wallpaperService.findByPage(pageable);
 
         model.addAttribute("content", page.getContent());
